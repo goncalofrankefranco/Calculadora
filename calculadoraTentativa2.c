e8f25c0d8a7cce23520d0ab6a0934b2a6006d8de
goncalofrankefranco <goncalofrankefranco@gmail.com>
2025-11-01 12:42:55 -0300

    Create calculadoraTentativa2.c

---
+++ calculadoraTentativa2.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <string.h>
+double x = 0, y = 0, a = 0, b = 0;
+char c;
+
+
+int adicao() {
+    printf("Digite o primeiro número: ");
+    scanf("%lf", &a);
+    getchar();
+    printf("\nDigite o segundo número: ");
+    scanf("%lf", &b);
+    getchar();
+    printf("\n(Se escreveu uma entrada não numérica, por favor realizar a conta novamente)");
+    printf("\nResultado: %lf + %lf = %lf", a, b, a + b);
+    printf("\nDeseja realizar outra operação? (s/n): ");
+    scanf("%c", &c);
+    do {
+        if (c == 's') {
+            return 0;
+        } else if (c == 'n') {
+            printf("Obrigado por usar a calculadora! Até a próxima.\n");
+            return 1;
+        } else {
+                printf("Sua opção é inválida.\nFavor escolher outra opção: ");
+                scanf("%c", &c);
+                getchar();
+            }
+        } while (c != 's' && c != 'n');
+}
+
+int subtracao() {
+    printf("Digite o primeiro número: ");
+    scanf("%lf", &a);
+    getchar();
+    printf("\nDigite o segundo número: ");
+    scanf("%lf", &b);
+    getchar();
+    printf("\n(Se escreveu uma entrada não numérica, por favor realizar a conta novamente)");
+    printf("\nResultado: %lf - %lf = %lf", a, b, a - b);
+    printf("\nDeseja realizar outra operação? (s/n): ");
+    scanf("%c", &c);
+    do {
+        if (c == 's') {
+            return 0;
+        } else if (c == 'n') {
+            printf("Obrigado por usar a calculadora! Até a próxima.\n");
+            return 1;
+        } else {
+                printf("Sua opção é inválida.\nFavor escolher outra opção: ");
+                scanf("%c", &c);
+                getchar();
+            }
+        } while (c != 's' && c != 'n');
+}
+
+int multiplicacao() {
+    printf("Digite o primeiro número: ");
+    scanf("%lf", &a);
+    getchar();
+    printf("\nDigite o segundo número: ");
+    scanf("%lf", &b);
+    getchar();
+    printf("\n(Se escreveu uma entrada não numérica, por favor realizar a conta novamente)");
+    printf("\nResultado: %lf * %lf = %lf", a, b, a * b);
+    printf("\nDeseja realizar outra operação? (s/n): ");
+    scanf("%c", &c);
+    do {
+        if (c == 's') {
+            return 0;
+        } else if (c == 'n') {
+            printf("Obrigado por usar a calculadora! Até a próxima.\n");
+            return 1;
+        } else {
+                printf("Sua opção é inválida.\nFavor escolher outra opção: ");
+                scanf("%c", &c);
+                getchar();
+            }
+        } while (c != 's' && c != 'n');
+}
+
+
+int divisao() {
+    printf("\nDigite o primeiro número: ");
+    scanf("%lf", &a);
+    getchar();
+    printf("\nDigite o segundo número: ");
+    scanf("%lf", &b);
+    getchar();
+    while (b == 0) {
+        if (b == 0) {
+            printf("\nNão é possível dividir por 0. Por favor escolha outro número: ");
+            scanf("%lf", &b);
+            getchar();
+        }
+    }
+    printf("\n(Se escreveu uma entrada não numérica, por favor realizar a conta novamente)");
+    printf("\nResultado: %lf/%lf = %lf", a, b, a/b);
+    printf("\nDeseja realizar outra operação? (s/n): ");
+    scanf("%c", &c);
+    do {
+        if (c == 's') {
+            return 0;
+        } else if (c == 'n') {
+            printf("Obrigado por usar a calculadora! Até a próxima.\n");
+            return 1;
+        } else {
+                printf("Sua opção é inválida.\nFavor escolher outra opção: ");
+                scanf("%c", &c);
+                getchar();
+            }
+        } while (c != 's' && c != 'n');
+}
+
+
+int main() {
+    while (x != 1 || x != 2 || x != 3 || x != 4 || x != 5) {
+        printf("===============================\nCalculadora Simples\n===============================\nSelecione uma operação:\n1. Adição\n2. Subtração\n3. Multiplicação\n4. Divisão\n5. Sair\nOpção: ");
+        scanf("%lf", &x);
+        getchar();
+        if (x == 1) {
+            if (adicao()) {
+                return 0;
+            }
+        } else if (x == 2) {     
+            if (subtracao()) {
+                return 0;
+            }          
+        } else if (x == 3) {    
+            if (multiplicacao()) {
+                return 0;
+            }      
+        } else if (x == 4) {      
+            if (divisao()) {
+                return 0;
+            }        
+        } else if (x == 5) {
+            printf("\nObrigado por usar a calculadora! Até a próxima.\n");
+            return 0;
+        } else {
+            printf("\nSua opção é inválida.\n Favor prossegir e em seguida escolher outra opção: ");
+            scanf("");
+            getchar();
+        }
+    }
+    x = 0;
+}
